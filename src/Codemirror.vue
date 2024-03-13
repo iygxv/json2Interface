9788a73cdf3ac1dc8edd0695a9dbd1f143608d43
sy <871772825@qq.com>
2024-03-13 18:16:01 +0800

    feat：overall interface optimization

---
+++ src/Codemirror.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+  import { Codemirror } from 'vue-codemirror';
+  import { defineProps, ref } from 'vue';
+  import { javascript } from '@codemirror/lang-javascript';
+  import { json } from '@codemirror/lang-json';
+  import { Debounced } from "@/utils";
+  // import { oneDark } from '@codemirror/theme-one-dark';
+
+  // 定义props
+  const props = defineProps({
+    code: String,
+    languages: String,
+    disabled: Boolean
+  });
+
+  const emit = defineEmits(['codeChange'])
+
+  const languagesMap: any = {
+    javascript: javascript(),
+    json: json(),
+  }
+  const targetLanguages = props.languages || 'javascript'
+  const editorExtensions = ref([
+    languagesMap[targetLanguages],
+    // oneDark
+  ])
+
+  console.log(editorExtensions);
+  
+
+  // 事件处理函数
+  const handleCodeMirrorReady = () => {
+    // 处理Codemirror准备就绪的逻辑
+  };
+
+  const handleCodeChange = (event: any) => {
+    // 处理代码变化的逻辑
+    if(props.languages ==='json') {
+      Debounced.use(() => codeChange(event))()
+    }
+  };
+
+  const codeChange = (code: any) => {
+    emit('codeChange', code)
+  }; 
+
+  const handleCodeFocus = (event: any) => {
+    console.log('focus', event);
+    // 处理代码编辑器获得焦点的逻辑
+  };
+
+  const handleCodeBlur = (event: any) => {
+    console.log('blur', event);
+    // 处理代码编辑器失去焦点的逻辑
+  };
+</script>
+
+<template>
+  <Codemirror
+    :modelValue="code"
+    :placeholder="`${props.languages === 'json' ? '请输入 JSON 数据' : '' }`"
+    class="codemirror"
+    :disabled="props.disabled"
+    :autofocus="true"
+    :indent-with-tab="true"
+    :tab-size="2"
+    :extensions="editorExtensions"
+    @change="handleCodeChange"
+  />
+</template>
+<style scoped lang="scss">
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import GitHub from './icons/GitHub.vue'
+</script>
+
+<template>
+  <nav>
+    <h1>
+      <img alt="logo" src="./assets/vite.svg" />
+      <span>Json To Interface</span>
+    </h1>
+    <div class="links">
+      <a
+        href="https://github.com/iygxv/json2Interface"
+        target="_blank"
+        title="View on GitHub"
+        class="github"
+      >
+        <GitHub />
+      </a>
+    </div>
+  </nav>
+</template>
+
+<style>
+nav {
+  --bg: #fff;
+  --bg-light: #fff;
+  --border: #ddd;
+  --btn: #666;
+  --highlight: #333;
+  --green: #3ca877;
+  --purple: #904cbc;
+  --btn-bg: #eee;
+
+  color: var(--base);
+  height: var(--nav-height);
+  box-sizing: border-box;
+  padding: 0 10px !important;
+  background-color: var(--bg);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.33);
+  position: relative;
+  z-index: 999;
+  display: flex;
+  justify-content: space-between;
+}
+
+.dark nav {
+  --base: #ddd;
+  --bg: #1a1a1a;
+  --bg-light: #242424;
+  --border: #383838;
+  --highlight: #fff;
+  --btn-bg: #333;
+
+  box-shadow: none;
+  border-bottom: 1px solid var(--border);
+}
+
+h1 {
+  font-weight: 500;
+  display: inline-flex;
+  place-items: center;
+}
+
+h1 img {
+  height: 24px;
+  margin-right: 10px;
+}
+
+@media (max-width: 560px) {
+  h1 span {
+    font-size: 0.9em;
+  }
+}
+
+@media (max-width: 520px) {
+  h1 span {
+    display: none;
+  }
+}
+
+.links {
+  display: flex;
+}
+
+
+.links .github {
+  padding: 1px 6px;
+  color: var(--btn);
+}
+
+.links .github:hover {
+  color: var(--highlight);
+}
+
+.links > * {
+  display: flex;
+  align-items: center;
+}
+
+.links > * + * {
+  margin-left: 4px;
+}
+</style>
